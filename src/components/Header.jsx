ee75ac8225160bd7f6a48c51aa16090cfb58f86e
Sourav Sinha <70857356+Souravsinha96@users.noreply.github.com>
2020-12-20 18:42:25 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import EventNoteTwoToneIcon from "@material-ui/icons/EventNoteTwoTone";
+function Header() {
+  return (
+    <header>
+      <h1>
+        <EventNoteTwoToneIcon />
+        Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
